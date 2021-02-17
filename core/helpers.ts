9876e747eb3b2114bb14e15cb31704f125f6c49a
NickHaecker <nick.philipp.haecker@hs-furtwangen.de>
2021-02-17 23:55:42 +0100

    neuer stand bei aboutMe

--- core/helpers.ts
+++ core/helpers.ts
@@ -6,6 +6,14 @@ export function RouteMapping(name: string): string {
       return 'homepage'
   }
 }
+export function TypeMapping(name: string): string {
+  switch (name) {
+    case 'index':
+      return 'homepage'
+    default:
+      return 'homepage'
+  }
+}
 export function QueryString(object: any): string {
   const routeParams: string = Object.keys(object)
     .map((key: string) => key + '=' + object[key])
--- theme/slicezone/slices/aboutMe.vue
+++ theme/slicezone/slices/aboutMe.vue
@@ -6,6 +6,36 @@
         :data="slice.primary.abschnittsuberschrift"
       />
       <rich-text :data="slice.primary.aboutme" class="about-me-description" />
+      <div class="sub-wrapper">
+        <div class="button-left-wrapper">
+          <rich-text
+            class="button-left-text"
+            :data="slice.primary.buttonlefttext"
+          />
+          <Button
+            class="button-arrow-left"
+            button-text="zum Lebenslauf"
+            icon="mdi-arrow-left"
+            :left="true"
+            :right="false"
+            route-link="/lebenslauf"
+          />
+        </div>
+        <div class="button-right-wrapper">
+          <rich-text
+            class="button-right-text"
+            :data="slice.primary.buttonrighttext"
+          />
+          <Button
+            class="button-arrow-right"
+            button-text="zum Kontakt"
+            icon="mdi-arrow-right"
+            :left="false"
+            :right="true"
+            route-link="/kontakt"
+          />
+        </div>
+      </div>
     </v-container>
   </div>
 </template>
@@ -13,10 +43,8 @@
 <script>
 import SliceProps from '../../../core/sliceProps'
 import Components from '../../../core/components'
-// import richText from '../richText.vue'
 export default {
   name: 'AboutMe',
-  //   components: { richText },
   mixins: [SliceProps, Components],
 }
 </script>
@@ -27,16 +55,206 @@ export default {
   .container {
     .about-me-title {
       color: white;
-      position: relative;
       text-align: center;
-      margin-top: -75px;
       font-size: 30px;
       font-weight: normal;
     }
     .about-me-description {
-      padding-top: 25px;
+      color: white;
+    }
+    .sub-wrapper {
+      .button-left-wrapper {
+        color: white;
+        .button-left-text {
+        }
+        .button-arrow-left {
+        }
+      }
+      .button-right-wrapper {
+        color: white;
+        .button-right-text {
+        }
+        .button-arrow-right {
+        }
+      }
+    }
+  }
+  @media (max-width: 425px) {
+    .container {
+      .about-me-title {
+      }
+      .about-me-description {
+      }
+      .sub-wrapper {
+        .button-left-wrapper {
+          .button-left-text {
+          }
+          .button-arrow-left {
+          }
+        }
+        .button-right-wrapper {
+          .button-right-text {
+          }
+          .button-arrow-right {
+          }
+        }
+      }
+    }
+  }
+  @media (min-width: 768px) {
+    .container {
+      .about-me-title {
+      }
+      .about-me-description {
+      }
+      .sub-wrapper {
+        .button-left-wrapper {
+          .button-left-text {
+          }
+          .button-arrow-left {
+          }
+        }
+        .button-right-wrapper {
+          .button-right-text {
+          }
+          .button-arrow-right {
+          }
+        }
+      }
+    }
+  }
+  @media (min-width: 1024px) {
+    .container {
+      .about-me-title {
+        position: relative;
+        margin-top: -75px;
+      }
+      .about-me-description {
+        padding-top: 25px;
+        min-height: 300px;
+      }
+      .sub-wrapper {
+        display: flow-root;
+        .button-left-wrapper {
+          float: left;
+          min-width: 300px;
+          .button-left-text {
+          }
+          .button-arrow-left {
+          }
+        }
+        .button-right-wrapper {
+          float: right;
+          min-width: 300px;
+          .button-right-text {
+          }
+          .button-arrow-right {
+          }
+        }
+      }
     }
   }
 }
 </style>
-<style lang="scss"></style>
+<style lang="scss">
+.about-me {
+  @media (max-width: 425px) {
+    .container {
+      .about-me-title {
+      }
+      .about-me-description {
+      }
+      .sub-wrapper {
+        .button-left-wrapper {
+          .button-left-text {
+          }
+          .button-arrow-left {
+          }
+        }
+        .button-right-wrapper {
+          .button-right-text {
+          }
+          .button-arrow-right {
+          }
+        }
+      }
+    }
+  }
+  @media (min-width: 768px) {
+    .container {
+      .about-me-title {
+      }
+      .about-me-description {
+      }
+      .sub-wrapper {
+        .button-left-wrapper {
+          .button-left-text {
+          }
+          .button-arrow-left {
+          }
+        }
+        .button-right-wrapper {
+          .button-right-text {
+          }
+          .button-arrow-right {
+          }
+        }
+      }
+    }
+  }
+  @media (min-wdith: 1024px) {
+    .container {
+      .about-me-title {
+      }
+      .about-me-description {
+      }
+      .sub-wrapper {
+        .button-left-wrapper {
+          .button-left-text {
+          }
+          .button-arrow-left {
+          }
+        }
+        .button-right-wrapper {
+          .button-right-text {
+          }
+          .button-arrow-right {
+          }
+        }
+      }
+    }
+  }
+  .container {
+    .about-me-title {
+    }
+    .about-me-description {
+    }
+    .sub-wrapper {
+      .button-left-wrapper {
+        .button-left-text {
+        }
+        .button-arrow-left {
+          color: black !important;
+          .button {
+            background-color: white;
+            border-radius: 15px;
+            border: 1px white solid;
+          }
+        }
+      }
+      .button-right-wrapper {
+        .button-right-text {
+        }
+        .button-arrow-right {
+          color: black !important;
+          .button {
+            background-color: white;
+            border-radius: 15px;
+            border: 1px white solid;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
